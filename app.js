26337174296b46012d09e834225fa79cb8fd9195
RichA <rich_ay@sbcglobal.net>
2020-09-19 10:06:27 -0500

    Have first 'inquirer' question working.

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 
 /* JavaScript file for Portfolio-Generator project. */
 
-const fs           = require( 'fs' );                           /* Include the 'file system' module */
-const generatePage = require( './src/page-template.js' );       /* Import from a local module */
+const inquirer = require( 'inquirer' );   /* import the 'inquirer' module, loaded by 'npm' */
+
+//console.log( inquirer );
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-//////// *** This is a sample routine to show argument handling, arrow functions
-//////// *** and 'forEach'
-/////////////////////////////////////////////////////////////////////////////////////////////
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log( profileDataArgs);
+/* ////////////////////////////////////////////////////// 
+   Question Section                                    */
 
-// const printProfileData = profileDataArr => {
-//      profileDataArr.forEach( profileItem => console.log(profileItem));
-// };
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then( answers => console.log( answers ) );
     
-
-// printProfileData(profileDataArgs);
-
-
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const uname   = profileDataArgs[0];
-// const github = profileDataArgs[1];
-
-const [name, github] = profileDataArgs;
-
-
-// Write out the generated HTML page to a file.
-
-fs.writeFile( 'index.html', generatePage( name, github ), err => {
-    if( err ) throw err;
-
-    console.log( 'Portfolio complete! Check out index.html to see the output!' );
-});
